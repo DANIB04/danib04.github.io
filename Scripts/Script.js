163383fb5d24ddb3ec9066d7516758ebf7392f47
DANIB04 <150392853+DANIB04@users.noreply.github.com>
2024-07-03 22:13:41 +0200

    pqawimdpawmfpawmfpamf

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -1,6 +1,7 @@
 function toggleTheme() {
     const body = document.body;
-    body.dataset.theme = body.dataset.theme === 'dark' ? 'light' : 'dark';
+    const newTheme = body.dataset.theme === 'dark' ? 'light' : 'dark';
+    body.dataset.theme = newTheme;
 }
 
 function showContent(section) {
@@ -29,4 +30,33 @@ document.addEventListener('DOMContentLoaded', function() {
 
     document.body.dataset.theme = 'dark';
     showContent('home');
+    updateCountdown();
 });
+
+var countdowns = [
+    { id: 1, targetDate: new Date("2024/12/23 00:00:00") },
+    { id: 2, targetDate: new Date("2024/10/14 00:00:00") }
+];
+
+function updateCountdown() {
+    countdowns.forEach(countdown => {
+        var currentDate = new Date();
+        var diff = (countdown.targetDate - currentDate) / 1000;
+
+        var days = Math.floor(diff / (24 * 60 * 60));
+        diff -= days * 24 * 60 * 60;
+
+        var hours = Math.floor(diff / (60 * 60));
+        diff -= hours * 60 * 60;
+
+        var minutes = Math.floor(diff / 60);
+        var seconds = Math.floor(diff - minutes * 60);
+
+        document.getElementById('days' + countdown.id).querySelector('.number').textContent = days;
+        document.getElementById('hours' + countdown.id).querySelector('.number').textContent = hours;
+        document.getElementById('minutes' + countdown.id).querySelector('.number').textContent = minutes;
+        document.getElementById('seconds' + countdown.id).querySelector('.number').textContent = seconds;
+    });
+}
+
+setInterval(updateCountdown, 1000);
