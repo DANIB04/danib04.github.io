f405d7f8bd5565e576bf6e7aa0d207e6d27e9a78
DANIB04 <bdanielbiju@gmail.com>
2024-09-28 23:20:02 +0200

    Cambios de estructura menu (1260)

--- JS/Index.js
+++ JS/Index.js
@@ -1,15 +1,11 @@
-// script.js
 
-// Función para cargar la página en el iframe
 function loadPage(page, activeLink) {
     const contentFrame = document.getElementById('contentFrame');
-    contentFrame.src = page; // Cambia la fuente del iframe a la página deseada
+    contentFrame.src = page;
 
-    // Eliminar la clase 'active' de todos los enlaces
     const links = document.querySelectorAll('.nav-links-custom a');
     links.forEach(link => link.classList.remove('active'));
 
-    // Añadir la clase 'active' al enlace correspondiente
     const activeElement = Array.from(links).find(link => 
         link.getAttribute('onclick').includes(activeLink)
     );
@@ -19,20 +15,18 @@ function loadPage(page, activeLink) {
     }
 }
 
-// Inicializa la página cargando el contenido por defecto
 document.addEventListener('DOMContentLoaded', function () {
-    loadPage('home.html', 'home.html'); // Carga la sección por defecto
+    loadPage('home.html', 'home.html');
 });
 
 document.getElementById('hamburger').addEventListener('click', function() {
 	const navLinks = document.getElementById('nav-links');
-	navLinks.classList.toggle('active'); // Alterna la clase 'active' para mostrar/ocultar el menú
+	navLinks.classList.toggle('active');
 });
 
-// Agrega evento a cada enlace para cerrar el menú al seleccionarlo
 document.querySelectorAll('.nav-links-custom a').forEach(link => {
 	link.addEventListener('click', function() {
 		const navLinks = document.getElementById('nav-links');
-		navLinks.classList.remove('active'); // Oculta el menú
+		navLinks.classList.remove('active'); 
 	});
 });
